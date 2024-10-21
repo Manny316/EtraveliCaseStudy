5fa91144cc793f69269a686aa7266229dd030b82
Manny316 <fotopoulosalexandros2@gmail.com>
2024-10-21 22:29:13 +0300

    Etraveli Case Study

--- src/test/java/TestCases/TestUsingFilters.java
+++ src/test/java/TestCases/TestUsingFilters.java
@@ -11,22 +11,20 @@ public class TestUsingFilters extends Base {
         travelBO.verifyNumberOfFlights("Showing 387 of 387 flights");
     }
 
-
     @Test(alwaysRun = true, description = "Update Filters(Number of Stops,Price,Airlines,Departure Time,Arrival Time,Travel Time),Verify results include only selected Flight Company", dependsOnMethods = "testStep_1")
     public void testStep_2() throws InterruptedException {
         travelBO.filterFlight();
         travelBO.verifyCompanyName("Aegean Airlines");
     }
 
-    @Test(alwaysRun = true, description = "Update Filters(Number of Stops,Price,Airlines,Departure Time,Arrival Time,Travel Time),Verify results include only selected range of Departure Times", dependsOnMethods = "testStep_2")
+    @Test(alwaysRun = true, description = "Verify results include only selected range of Departure Times", dependsOnMethods = "testStep_2")
     public void testStep_3() {
         // outgoing flight ATH - THES : max 08:30 departure time -> verify departure time < 9
         // ingoing flight THES - ATH : min 10:00 arrival time -> verify departure time > 9
         travelBO.verifyDepartureTime(9, 9);
     }
 
-
-    @Test(alwaysRun = true, description = "Update Filters(Number of Stops,Price,Airlines,Departure Time,Arrival Time,Travel Time),Verify results include only selected range of Flight Duration", dependsOnMethods = "testStep_3")
+    @Test(alwaysRun = true, description = "Verify results include only selected range of Flight Duration", dependsOnMethods = "testStep_3")
     public void testStep_4(){
         // max flight duration 2:00h
         travelBO.verifyDurationTime(120, true);
